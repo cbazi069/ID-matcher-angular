965180ae688cf3853688b805b28fccab57f2073f
Clément Bazin <cbazi069@uottawa.ca>
2019-07-18 14:14:18 -0400

    Add selection sur les carte et mousewheel

--- src/app/image-display/image-display.component.ts
+++ src/app/image-display/image-display.component.ts
@@ -10,31 +10,51 @@ export class ImageDisplayComponent implements OnInit {
   @Input() personToDisplay: Person;
   myStyle: any = {};
   url = '';
+  selected = false;
+  IndexToDisplay = 0;
 
   constructor() {
   }
 
   ngOnInit() {
-    console.log(this.personToDisplay);
-    var reader = new FileReader();
-    reader.readAsDataURL(this.personToDisplay.frames[1].file); // read file as data url
-    reader.onload = (event) => { // called once readAsDataURL is completed
-      this.url = event.target["result"];
-    }
-    this.CropingXYtoPixels();
+    this.printImage();
   }
 
   CropingXYtoPixels() {
-    var annotation = this.personToDisplay.annotations[1];
+    var annotation = this.personToDisplay.annotations[this.IndexToDisplay];
     var height = annotation.height;
     var width = annotation.width;
     var x = -annotation.x;
     var y = -annotation.y;
     this.myStyle = {
       "object-fit": "none",
-      "object-position": x.toString()+"px "+y.toString()+"px",
-      "width": width.toString() +"px",
-      "height": height.toString() +"px",
+      "object-position": x.toString() + "px " + y.toString() + "px",
+      "width": width.toString() + "px",
+      "height": height.toString() + "px",
     }
+  }
+  TogglePerson(): void {
+    this.selected = !this.selected;
+  }
+
+  mouseWheelUpFunc() {
+    if (this.IndexToDisplay + 10 < this.personToDisplay.annotations.length) {
+      this.IndexToDisplay+=10;      
+      this.printImage();
+    }
+  }
+  mouseWheelDownFunc() {
+    if (this.IndexToDisplay - 10 >= 0) {
+      this.IndexToDisplay-=10;
+      this.printImage();
+    }
+  }
+  printImage() {
+    var reader = new FileReader();
+    reader.readAsDataURL(this.personToDisplay.frames[this.IndexToDisplay].file); // read file as data url
+    reader.onload = (event) => { // called once readAsDataURL is completed
+      this.url = event.target["result"];
+    }
+    this.CropingXYtoPixels();
   }
 }
