03e8535de1a0e8b1122aec33f87bff7e18da5029
Clément Bazin <cbazi069@uottawa.ca>
2019-07-24 12:11:28 -0400

    ajout du bouton not a person

--- src/app/buttons-container/buttons-container.component.ts
+++ src/app/buttons-container/buttons-container.component.ts
@@ -31,6 +31,28 @@ export class ButtonsContainerComponent implements OnInit {
       this.BtnMergeClicked.emit(null);
     }
   }
+
+
+  NotAPerson() {
+    if (this.assets.SelectedPeople.Folder1.length + this.assets.SelectedPeople.Folder2.length + this.assets.SelectedPeople.BottomBar.length < 1) {
+      alert("Please select at least 1 person!")
+    }
+    else {
+      for (var person1 of this.assets.SelectedPeople.Folder1) {
+        person1.used = true;
+      }
+      for (var person2 of this.assets.SelectedPeople.Folder2) {
+        person2.used = true;
+      }
+      this.assets.SelectedPeople.Reset();
+      this.BtnMergeClicked.emit(null);
+    }
+  }
+
+
+
+
+
   ngOnInit() {
   }
 
