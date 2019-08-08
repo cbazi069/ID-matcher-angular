2eef8f305dd82eac90b32b261a7d492958223179
Clément Bazin <cbazi069@uottawa.ca>
2019-08-08 13:17:35 -0400

    same size image begin

--- src/app/image-display/image-display.component.ts
+++ src/app/image-display/image-display.component.ts
@@ -46,15 +46,20 @@ export class ImageDisplayComponent implements OnInit {
 
   CropingXYtoPixels() {
     var annotation = this.personToDisplay.annotations[this.IndexToDisplay];
+    let standard_width = 110;
+    let standard_height = 160;
     var height = annotation.height;
     var width = annotation.width;
     var x = -annotation.x;
     var y = -annotation.y;
     this.myStyle = {
+      "background-image": "url(" + this.url + ")",
+      "background-size": standard_width / annotation.width * 640 + "px " + standard_height / annotation.width * 480 + "px ",
+      "background-repeat": "no-repeat",
       "object-fit": "none",
-      "object-position": x.toString() + "px " + y.toString() + "px",
-      "width": width.toString() + "px",
-      "height": height.toString() + "px",
+      "background-position": x.toString() + "px " + y.toString() + "px",
+      "width": standard_width + "px",
+      "height": standard_height + "px",
     }
   }
   TogglePerson(): void {
@@ -78,15 +83,15 @@ export class ImageDisplayComponent implements OnInit {
 
 
   mouseWheelUpFunc() {
-    if (this.IndexToDisplay + 10 < this.personToDisplay.annotations.length) {
-      this.IndexToDisplay+=10;      
+    if (this.IndexToDisplay + 3 < this.personToDisplay.annotations.length) {
+      this.IndexToDisplay+=3;      
       this.printImage();
     }
     
   }
   mouseWheelDownFunc() {
-    if (this.IndexToDisplay - 10 >= 0) {
-      this.IndexToDisplay-=10;
+    if (this.IndexToDisplay - 3 >= 0) {
+      this.IndexToDisplay-=3;
       this.printImage();
     }
     
@@ -96,7 +101,7 @@ export class ImageDisplayComponent implements OnInit {
     reader.readAsDataURL(this.personToDisplay.frames[this.IndexToDisplay].file); // read file as data url
     reader.onload = (event) => { // called once readAsDataURL is completed
       this.url = event.target["result"];
+      this.CropingXYtoPixels();
     }
-    this.CropingXYtoPixels();
   }
 }
